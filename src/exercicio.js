f295ec996f2c2f43bd5c5aa7d85651bdb1244a41
sique <siqueira.natalicio08@gmail.com>
2024-12-31 14:39:43 -0300

    exercico type script

---
+++ src/exercicio.js
@@ -0,0 +1,12 @@
+"use strict";
+// Função de multiplicação
+function multiplicar(a, b) {
+    return a * b;
+}
+// Função de saudação
+function saudar(nome) {
+    return `Olá ${nome}`;
+}
+// Exemplos de uso:
+const resultadoMultiplicacao = multiplicar(5, 3); // retorna 15
+const saudacao = saudar("Maria"); // retorna "Olá Maria"
---
+++ src/exercicio.ts
@@ -0,0 +1,13 @@
+// Função de multiplicação
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+// Função de saudação
+function saudar(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+// Exemplos de uso:
+const resultadoMultiplicacao = multiplicar(5, 3);  // retorna 15
+const saudacao = saudar("Maria");  // retorna "Olá Maria"
